4b0868f4fd93bdce56fed67b21a6c6ae457265e5
annevem <annekaroine@live.com>
2025-11-07 21:35:36 -0300

    Aula 6.2

---
+++ ex.28.py
@@ -0,0 +1,4 @@
+numeros=["1","2","3","4", "6","7","8","9", "10"]
+print("Os numeros são: ")
+for numero in numeros:
+    print(f"- Numero {numero}.")
---
+++ ex27.py
@@ -0,0 +1,6 @@
+for i in range(0,5):
+    n=int(input("Digite um número:  "))
+    if n % 2 ==0:
+        print(f"{n} é par. ")
+    else:
+        print(f"{n} é impar. ")
